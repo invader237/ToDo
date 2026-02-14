f341ca7463575b6424d90cad1974c8af7d1038f4
invader237 <diego.trivino.fr@gmail.com>
2026-02-14 09:14:17 +0100

    :sparkles:(Tache): add default Tache managment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,33 @@
 package org.example;
 
+import org.example.tache.Tache;
+
+import java.awt.*;
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Class par défaut de l'application
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Choisissez une action :");
+        System.out.println("1. créer une tache");
+
+        Scanner sc = new Scanner(System.in);
+
+        int choice = sc.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Description : \n");
+            String description = sc.nextLine();
+
+            Tache tache = new Tache(description);
+
+            System.out.println(tache.toString());
+        }
+
     }
 }
---
+++ src/main/java/org/example/tache/Tache.java
@@ -0,0 +1,41 @@
+package org.example.tache;
+
+public class Tache {
+    private boolean done;
+    private String description;
+
+
+    /**
+     * Constructeur par défaut d'une tache
+     * Set le status de la tache a false par défaut ( la tache n'est pas réalisé)
+     * @param description la description de la tache
+     */
+    public Tache(String description) {
+        this.done = false;
+        this.description = description;
+    }
+
+    /**
+     * Inverse le status de la tache (comme une case à cocher)
+     */
+    public void setDoneStatus() {
+        this.done = !done;
+    }
+
+    /**
+     * Mettre à jour la description
+     * @param description la nouvelle description de la tache
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Affichage console d'une tache
+     * @return affichage console d'une tache avec un formatage
+     */
+    @Override
+    public String toString() {
+        return(this.description +  " - Done : " + this.done);
+    }
+}
